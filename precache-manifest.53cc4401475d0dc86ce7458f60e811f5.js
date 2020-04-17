e143c1989431268ef07f3108d1ca0fcb196b07ed
Wouter van den Heuvel <exo_00@hotmail.com>
2020-04-17 13:36:25 +0200

    Updates

---
+++ precache-manifest.53cc4401475d0dc86ce7458f60e811f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8540f0754f7b4658fb1b445f18be95f3",
+    "url": "/spassvogel/warehousemap/index.html"
+  },
+  {
+    "revision": "b9dac2093fae67d5b4ad",
+    "url": "/spassvogel/warehousemap/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "88431ef445a7c83d6a23",
+    "url": "/spassvogel/warehousemap/static/js/2.5b434798.chunk.js"
+  },
+  {
+    "revision": "555486c5c14e9d19a66b1506a7ff28b9",
+    "url": "/spassvogel/warehousemap/static/js/2.5b434798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9dac2093fae67d5b4ad",
+    "url": "/spassvogel/warehousemap/static/js/main.cc470794.chunk.js"
+  },
+  {
+    "revision": "391473b0d59c15c34d8e",
+    "url": "/spassvogel/warehousemap/static/js/runtime-main.cc584c6f.js"
+  }
+]);
