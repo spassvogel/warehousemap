5f40f1b0a38c63ef539e2b516f4583e72dc6de07
Wouter van den Heuvel <exo_00@hotmail.com>
2020-04-18 23:17:09 +0200

    added marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React, { useRef } from 'react';
+import * as PIXI from 'pixi.js';
 import logo from './logo.svg';
 import './App.css';
 import { Stage, Sprite } from '@inlet/react-pixi';
 import Viewport from './components/pixi/Viewport';
 import { Viewport as PixiViewport } from "pixi-viewport";
 import Marker from './components/pixi/Marker';
+
+if (process.env.NODE_ENV === "development") {
+  // @ts-ignore
+  window.__PIXI_INSPECTOR_GLOBAL_HOOK__ && window.__PIXI_INSPECTOR_GLOBAL_HOOK__.register({ PIXI: PIXI });
+}
 
 function App() {
   console.log(process.env.PUBLIC_URL);
@@ -20,7 +26,10 @@ function App() {
         <Viewport screenWidth={canvasWidth} screenHeight={canvasHeight} worldWidth={worldWidth} worldHeight={worldHeight} ref={viewportRef} >
 
           <Sprite image={`${process.env.PUBLIC_URL}/map.png`} />
-          <Marker />
+
+
+          <Marker position={new PIXI.Point(440, 449)} />
+          <Marker position={new PIXI.Point(986, 724)} />
     </Viewport> 
   </Stage>
   );
--- src/components/pixi/Marker.tsx
+++ src/components/pixi/Marker.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Sprite } from '@inlet/react-pixi';
+import * as PIXI from 'pixi.js';
 
-const Marker = (props: React.ComponentProps<typeof Sprite>) => {
+interface Props {
+    position?: PIXI.Point;
+}
+
+const Marker = (props: Props & React.ComponentProps<typeof Sprite>) => {
+    const ref = useRef<PIXI.Sprite>(null);
+    const data = useRef<PIXI.interaction.InteractionData>();
+    const [position, setPosition] = useState<PIXI.Point>(props.position || new PIXI.Point());
+
+    const onDragStart = (event: PIXI.interaction.InteractionEvent) => {
+        // store a reference to the data
+        // the reason for this is because of multitouch
+        // we want to track the movement of this particular touch
+        data.current = event.data;
+        console.log(event)
+        event.stopPropagation(); // Stop dragging the map!
+    }
+    
+    const onDragEnd = () => {
+        data.current = undefined;
+        console.log(position);
+    }
+    
+    function onDragMove()
+    {
+        if (data.current)
+        {
+            const newPosition = data.current.getLocalPosition(ref.current!.parent);
+            setPosition(newPosition);
+        }
+    }
+    
+
     return (
         <Sprite 
             { ...props }
+            width={54}
+            height={72}
+            position={position}
+            ref={ref}
+            interactive={true}
             image={`${process.env.PUBLIC_URL}/marker.svg`}
+            mousedown={onDragStart}
+            touchstart={onDragStart}
+            mouseup={onDragEnd}
+            mouseupoutside={onDragEnd}
+            mousemove={onDragMove}
+            touchmove={onDragMove}
         />
     );
 }
